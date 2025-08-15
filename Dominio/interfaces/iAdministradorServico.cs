c89b6584efe69af75558221c60ef6e9e2ce02d4a
SchwartzLucas <lucassdesouza10@gmail.com>
2025-08-15 16:00:23 -0300

    Criada interface do AdministradorServico

---
+++ Dominio/interfaces/iAdministradorServico.cs
@@ -0,0 +1,8 @@
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.DTOs;
+
+namespace MinimalApi.Dominio.Interfaces;
+
+public interface iAdministradorServico{
+    Administrador? Login(LoginDTO loginDTO);
+}
